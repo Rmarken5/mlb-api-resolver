c4c23e81026081d26ad5f4be231015f17edea892
Ryan Marken <marken.ryan@gmail.com>
2019-05-14 22:26:22 -0400

    Add endpoint to get all teams

--- src/controllers/baseballController.js
+++ src/controllers/baseballController.js
@@ -18,7 +18,7 @@ exports.listTodaysGameFetch = function (req, res) {
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
-    const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1,51&date=${year}-${month}-${day}&gameTypes=E,S,R,A,F,D,L,W&hydrate=team(leaders(showOnPreview(leaderCategories=[homeRuns,runsBattedIn,battingAverage],statGroup=[pitching,hitting]))),linescore(matchup,runners),flags,liveLookin,review,broadcasts(all),decisions,person,probablePitcher,stats,homeRuns,previousPlay,game(content(media(featured,epg),summary),tickets),seriesStatus(useOverride=true)&useLatestGames=false&language=en&leagueId=103,104,420`
+    const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1,51&date=${year}-${month}-${day}&gameTypes=E,S,R,A,F,D,L,W&hydrate=team(leaders(showOnPreview(leaderCategories=[homeRuns,runsBattedIn,battingAverage],statGroup=[pitching,hitting]))),linescore(matchup,runners),flags,liveLookin,review,broadcasts(all),decisions,person,probablePitcher,stats,homeRuns,previousPlay,game(content(media(featured,epg),summary),tickets),seriesStatus(useOverride=true)&useLatestGames=false&language=en&leagueId=103,104,420`;
     fetch(url)
         .then(result => result.json())
         .then(json => {
@@ -31,5 +31,23 @@ exports.listTodaysGameFetch = function (req, res) {
                 res.json(json);
             }
         });
+
+}
+
+exports.getAllTeamsOnDate = function (req, res) {
+    ;
+    let date = new Date();
+    if (req.query &&
+        req.query['date'] &&
+        req.query['date'].match('\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*')) {
+        date = new Date(req.query['date']);
+    }
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const url = `https://statsapi.mlb.com/api/v1/schedule?sportId=1,51&date=${year}-${month}-${day}&gameTypes=E,S,R,A,F,D,L,W&hydrate=team(leaders(showOnPreview(leaderCategories=[homeRuns,runsBattedIn,battingAverage],statGroup=[pitching,hitting]))),linescore(matchup,runners),flags,liveLookin,review,broadcasts(all),decisions,person,probablePitcher,stats,homeRuns,previousPlay,game(content(media(featured,epg),summary),tickets),seriesStatus(useOverride=true)&useLatestGames=false&language=en&leagueId=103,104,420`;
+    fetch(url).then(result => result.json()).then(json => {
+        res.json(analyzeData.searchForTeams(json.dates[0]));
+    });
 
 }
